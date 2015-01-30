e8cbccfe912026b4a4b82cd761d9b4b324060e78
Dmitry Korunov <dessel.k@gmail.com>
2015-01-30 15:26:16 +0300

    added crime layout-land

--- app/src/main/java/criminalintent/k/dessel/ru/criminalintent/CrimeFragment.java
+++ app/src/main/java/criminalintent/k/dessel/ru/criminalintent/CrimeFragment.java
@@ -7,13 +7,20 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
 
 
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +48,24 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        if (mDateButton != null)
+        {
+            SimpleDateFormat df = new SimpleDateFormat("EEEE, MMM dd, yyyy H:mm");
+            mDateButton.setText(df.format(mCrime.getDate()));
+            mDateButton.setEnabled(false);
+        }
+
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 

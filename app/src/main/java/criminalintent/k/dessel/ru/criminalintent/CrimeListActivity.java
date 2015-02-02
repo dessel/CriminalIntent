4416e4a699df2c71d679acf5bfa814fc73fffcfb
Dmitry Korunov <dessel.k@gmail.com>
2015-02-02 14:27:42 +0300

    added abstract class for single fragment added crime list

--- app/src/main/java/criminalintent/k/dessel/ru/criminalintent/CrimeListActivity.java
+++ app/src/main/java/criminalintent/k/dessel/ru/criminalintent/CrimeListActivity.java
@@ -2,10 +2,10 @@ package criminalintent.k.dessel.ru.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
 
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }

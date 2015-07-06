a58a9a31879f7164c702f87b0433f145e44ba960
Dmitry Korunov <dessel.k@gmail.com>
2015-07-06 10:24:57 +0300

    added save data to JSON

--- app/src/main/java/ru/k/dessel/criminalintent/Crime.java
+++ app/src/main/java/ru/k/dessel/criminalintent/Crime.java
@@ -1,10 +1,18 @@
 package ru.k.dessel.criminalintent;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
+
+    private static final String JSON_ID     = "id";
+    private static final String JSON_TITLE  = "title";
+    private static final String JSON_SOLVED = "solved";
+    private static final String JSON_DATE   = "date";
 
     private UUID mId;
     private String mTitle;
@@ -31,6 +39,24 @@ public class Crime {
         // Генерирование уникального идентификатора
         mId = UUID.randomUUID();
         mDate = new Date();
+    }
+
+    public Crime(JSONObject json) throws JSONException {
+        mId = UUID.fromString(json.getString(JSON_ID));
+        mTitle = json.getString(JSON_TITLE);
+        mSolved = json.getBoolean(JSON_SOLVED);
+        mDate = new Date(json.getLong(JSON_DATE));
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+
+        json.put(JSON_ID,     mId.toString());
+        json.put(JSON_TITLE,  mTitle);
+        json.put(JSON_SOLVED, mSolved);
+        json.put(JSON_DATE,   mDate.getTime());
+
+        return json;
     }
 
     public UUID getId() {
